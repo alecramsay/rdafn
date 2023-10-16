5395265c3940ec6a79645d4fbdd398623b5dc653
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-10-16 10:00:39 -0700

    WIP

--- rdafn/analyze.py
+++ rdafn/analyze.py
@@ -50,6 +50,7 @@ def analyze_plan(
     d_statewide: int = 0
     tot_statewide: int = 0
 
+    # NOTE - This could be done once for all plans being analyzed.
     counties: set[str] = set()
     districts: set[int | str] = set()
 
@@ -66,6 +67,7 @@ def analyze_plan(
     district_to_index: dict[int | str, int] = {
         district: i for i, district in enumerate(districts)
     }
+    # End NOTE
     CxD: list[list[float]] = [[0.0] * len(counties) for _ in range(len(districts))]
 
     for row in assignments:
@@ -93,11 +95,6 @@ def analyze_plan(
         j: int = county_to_index[county]
 
         CxD[i][j] += pop
-
-    Vf: float = d_statewide / tot_statewide
-    Vf_array: list[float] = [
-        d / tot for d, tot in zip(d_by_district.values(), tot_by_district.values())
-    ]
 
     pass
 
@@ -145,32 +142,75 @@ def analyze_plan(
     target_pop: int = int(total_pop / len(pop_by_district))
 
     deviation: float = rda.calc_population_deviation(max_pop, min_pop, target_pop)
-    scorecard["popDev"] = deviation
+    scorecard["population_deviation"] = deviation
 
     # Partisan metrics
 
-    partisan_metrics: dict = rda.calc_partisan_metrics(Vf, Vf_array)
+    Vf: float = d_statewide / tot_statewide
+    Vf_array: list[float] = [
+        d / tot for d, tot in zip(d_by_district.values(), tot_by_district.values())
+    ]
 
-    bias_metrics: dict = dict(partisan_metrics["bias"])
-    bias_metrics.pop("rvPoints", None)
-    bias_metrics.pop("gamma", None)
-    bias_metrics.pop("gSym", None)
-    bias_metrics["disproportionality"] = bias_metrics.pop("deviation")
+    partisan_metrics: dict = rda.calc_partisan_metrics(Vf, Vf_array)
+    bias_metrics: dict = dict()
+    bias_metrics["pr_seats"] = partisan_metrics["bias"]["bestS"]
+    bias_metrics["pr_pct"] = partisan_metrics["bias"]["bestSf"]
+    bias_metrics["estimated_seats"] = partisan_metrics["bias"]["estS"]
+    bias_metrics["estimated_seat_pct"] = partisan_metrics["bias"]["estSf"]
+    bias_metrics["pr_deviation"] = partisan_metrics["bias"]["deviation"]
+    bias_metrics["turnout_bias"] = partisan_metrics["bias"]["tOf"]
+    bias_metrics["fptp_seats"] = partisan_metrics["bias"]["fptpS"]
+    bias_metrics["seats_bias"] = partisan_metrics["bias"]["bS50"]
+    bias_metrics["votes_bias"] = partisan_metrics["bias"]["bV50"]
+    bias_metrics["declination"] = partisan_metrics["bias"]["decl"]
+    bias_metrics["global_symmetry"] = partisan_metrics["bias"]["gSym"]
+    bias_metrics["gamma"] = partisan_metrics["bias"]["gamma"]
+    bias_metrics["efficiency_gap"] = partisan_metrics["bias"]["eG"]
+    bias_metrics["geometric_seats_bias"] = partisan_metrics["bias"]["bSV"]
+    bias_metrics["disproportionality"] = partisan_metrics["bias"]["prop"]
+    bias_metrics["mean_median_statewide"] = partisan_metrics["bias"]["mMs"]
+    bias_metrics["mean_median_average_district"] = partisan_metrics["bias"]["mMd"]
+    bias_metrics["lopsided_outcomes"] = partisan_metrics["bias"]["lO"]
     scorecard.update(bias_metrics)
 
-    responsiveness_metrics: dict = dict(partisan_metrics["responsiveness"])
+    responsiveness_metrics: dict = dict()
+    responsiveness_metrics["competitive_districts"] = partisan_metrics[
+        "responsiveness"
+    ]["cD"]
+    responsiveness_metrics["competitive_district_pct"] = partisan_metrics[
+        "responsiveness"
+    ]["cDf"]
+    responsiveness_metrics["overall_responsiveness"] = partisan_metrics[
+        "responsiveness"
+    ][
+        "bigR"
+    ]  # BIG 'R': Defined in Footnote 22 on P. 10
+    responsiveness_metrics["responsiveness"] = partisan_metrics["responsiveness"][
+        "littleR"
+    ]
+    # responsiveness_metrics["minimal_inverse_responsiveness"] = partisan_metrics[
+    #     "responsiveness"
+    # ][
+    #     "mIR"
+    # ]  # zeta = (1 / r) - (1 / r_sub_max) : Eq. 5.2.1
+    responsiveness_metrics["responsive_districts"] = partisan_metrics["responsiveness"][
+        "rD"
+    ]
+    responsiveness_metrics["responsive_district_pct"] = partisan_metrics[
+        "responsiveness"
+    ]["rDf"]
     scorecard.update(responsiveness_metrics)
 
-    scorecard.update({"averageDVf": partisan_metrics["averageDVf"]})
-    scorecard.update({"averageRVf": partisan_metrics["averageRVf"]})
+    scorecard.update({"avg_dem_win_pct": partisan_metrics["averageDVf"]})
+    scorecard.update({"avg_rep_win_pct": partisan_metrics["averageRVf"]})
 
     # TODO - Minority
 
     # Compactness
 
     splitting_metrics: dict = rda.calc_county_district_splitting(CxD)
-    scorecard["countySplitting"] = splitting_metrics["county"]
-    scorecard["districtSplitting"] = splitting_metrics["district"]
+    scorecard["county_splitting"] = splitting_metrics["county"]
+    scorecard["district_splitting"] = splitting_metrics["district"]
 
     # TODO - Splitting
 
