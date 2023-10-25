28d1f892d93b936357838cdacbf05b95f09cf1f6
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-10-25 08:34:24 -0700

    WIP

--- sample.py
+++ sample.py
@@ -10,22 +10,27 @@ $ ./sample.py
 
 from rdafn import *
 
-xx: str = "NC"
-N: int = 14  # TODO - Get this from metadata
-C: int = 100  # TODO - Ditto
+# Specify a state and an ensemble of plans
 
-#
+xx: str = "NC"
+root_plan: str = path_to_file([data_dir, xx]) + "NC20C_baseline_100.csv"
+ensemble: list[str] = [root_plan]
+
+# Load the state
 
 data: dict[str, dict[str, int]] = load_data(xx)
 state_topo: dict[str, Any] = load_topology(xx)
+N: int = DISTRICTS_BY_STATE[xx]["congress"]
+C: int = COUNTIES_BY_STATE[xx]
 
-root_plan: str = path_to_file([data_dir, xx]) + "NC20C_baseline_100.csv"
-ensemble: list[str] = [root_plan]
+# Analyze each plan in the ensemble
 
 for plan_path in ensemble:
     assignments: list[dict[str, str | int]] = load_plan(plan_path)
 
     scorecard: dict[str, Any] = analyze_plan(assignments, data, state_topo, N, C)
+
+    # Do something with the resulting "scorecard"
 
     print()
     print(f"Scorecard:")
