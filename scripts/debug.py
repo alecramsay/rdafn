22e2449a3559d7ce18690162193e2e135caa0603
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-11-06 10:37:31 -0800

    Nit

--- scripts/debug.py
+++ scripts/debug.py
@@ -47,29 +47,12 @@ def main() -> None:
     args: Namespace = parse_args()
 
     xx: str = args.state
-    xx = "NJ"  # HACK
 
     verbose: bool = args.verbose
 
     #
 
-    plan_path: str = os.path.expanduser(
-        f"{data_dir}/{xx}/" + f"{xx}20C_baseline_100.csv"
-    )
-    plan: list[dict[str, str | int]] = load_plan(plan_path)
-
-    topo: dict[str, Any] = load_topology(xx)
-    D: int = DISTRICTS_BY_STATE[xx]["congress"]
-
-    #
-
-    district_geojsons: list[dict[str, Any]] = merge_features(topo, plan)
-
-    for i, d in enumerate(district_geojsons):
-        output_path: str = os.path.expanduser(
-            f"~/Downloads/{xx}/" + f"{xx}_district_{i+1}.geojson"
-        )
-        write_json(output_path, d)
+    print("Do something here.")
 
 
 if __name__ == "__main__":
